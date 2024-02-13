f7ef0a9f3a02536b9512a3c37f83cc5bd054d25c
Jesus <jcarbar634a@ieslacampina.es>
2024-02-13 14:32:50 +0100

    registrarXregistro

--- registrar.php
+++ registrar.php
@@ -6,6 +6,30 @@
     <title>Garaje - Registrar Usuario</title>
 </head>
 <body>
-    
-</body>
-</html>
+		<div class="container">
+			<div class="row">
+				<h1>Socios</h1>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-8">
+					<!-- Completar atributos de form -->
+					<form action="registrar2.php" id="registro" name="registro" autocomplete="off" method="post">
+
+						<label for="nombre">Nombre</label><input type="date" class="form-control" name="nombre" maxlength="50" id="nombre" required>
+                        <label for="Edad">Edad</label><input type="edad" class="form-control" name="edad" maxlength="50" id="edad" required>
+						
+						<div class="form-group">
+						<button type="submit" class="btn btn-primary">Registrar</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.4.1.min.js" ></script>
+		<script src="js/bootstrap.min.js" ></script>
+	</body>
+</html>			
