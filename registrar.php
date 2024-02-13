c95579839409df700e4ce91a64afa032bd1eeef2
Jesus <jcarbar634a@ieslacampina.es>
2024-02-13 14:53:48 +0100

    Añado Boostrap

--- registrar.php
+++ registrar.php
@@ -3,7 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Garaje - Registrar Usuario</title>
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">	
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js" ></script>
+	<script src="js/bootstrap.min.js" ></script>
+	<script src="js/jquery.dataTables.min.js" ></script>
+	
+    <title>Registro</title>
 </head>
 <body>
 		<div class="container">
